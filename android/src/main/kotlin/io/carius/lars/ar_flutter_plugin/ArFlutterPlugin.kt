68fd1bcdfb16be2a26825f0979726ebcf7889a9a
jingluoguo <jingluoguo@163.com>
2025-03-17 17:13:47 +0800

    添加接口isSupport

--- android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/ArFlutterPlugin.kt
+++ android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/ArFlutterPlugin.kt
@@ -1,8 +1,10 @@
 package io.carius.lars.ar_flutter_plugin
 
+import android.content.Context
+import android.util.Log
 import androidx.annotation.NonNull
+import com.google.ar.core.ArCoreApk
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
@@ -18,6 +20,7 @@ class ArFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel: MethodChannel
   private lateinit var flutterPluginBinding: FlutterPlugin.FlutterPluginBinding
+  private lateinit var context: Context
 
   override fun onAttachedToEngine(
       @NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding
@@ -26,12 +29,26 @@ class ArFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     channel.setMethodCallHandler(this)
 
     this.flutterPluginBinding = flutterPluginBinding
+    context = this.flutterPluginBinding.applicationContext
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
+    } else if (call.method == "isSupport") {
+      val value = when (ArCoreApk.getInstance().checkAvailability(context)) {
+        ArCoreApk.Availability.SUPPORTED_INSTALLED -> true // 设备支持且已安装 ARCore
+        ArCoreApk.Availability.SUPPORTED_APK_TOO_OLD,
+        ArCoreApk.Availability.SUPPORTED_NOT_INSTALLED -> {
+          // 设备支持但未安装 ARCore，或 ARCore 版本过旧
+          // 可以提示用户安装或更新 ARCore
+          false
+        }
+        else -> false // 设备不支持 ARCore
+      }
+      result.success(value)
+    }
+    else {
       result.notImplemented()
     }
   }
--- ios/Classes/SwiftArFlutterPlugin.swift
+++ ios/Classes/SwiftArFlutterPlugin.swift
@@ -1,18 +1,24 @@
 import Flutter
 import UIKit
+import ARKit
 
 public class SwiftArFlutterPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "ar_flutter_plugin", binaryMessenger: registrar.messenger())
-    let instance = SwiftArFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "ar_flutter_plugin", binaryMessenger: registrar.messenger())
+        let instance = SwiftArFlutterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        
+        let factory = IosARViewFactory(messenger: registrar.messenger())
+        registrar.register(factory, withId: "ar_flutter_plugin")
+    }
     
-    let factory = IosARViewFactory(messenger: registrar.messenger())
-    registrar.register(factory, withId: "ar_flutter_plugin")
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
-
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if (call.method == "getPlatformVersion") {
+            result("iOS " + UIDevice.current.systemVersion)
+        } else if (call.method == "isSupport") {
+            var res = ARConfiguration.isSupported
+            result(res)
+        }
+    }
+    
 }
